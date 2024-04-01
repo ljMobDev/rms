48c58d918ae1fe3d4391bd7b45641d02c3612a2c
lj123 <162970390+ljMobDev@users.noreply.github.com>
2024-04-01 17:34:47 +0800

    Update login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClientModule, HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-  username = ''
-  constructor() {
-    console.log(this.username);
+password=''
+
+loader:boolean = false;
+  constructor(private http: HttpClient){
+}
+
+login(){
+ this.http.get<any>(`https://webtechnologies.site/v2/agustin/api/login.php?username=`+this.username+`&password=`+this.password)
+        .subscribe((data: any) => {
+          console.log(data)
+          this.loader = false
+          if(data.message=="no record")
+            alert(data.message)
+          else{
+            alert(data.message)
+          }
+        });
  
    }
  isToastOpen = false;
